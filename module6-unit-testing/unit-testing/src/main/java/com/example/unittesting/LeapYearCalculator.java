dc0848a95532f8eb5624a119c0489331de8af5b5
aigulsharip <aigul.sharip@nu.edu.kz>
2026-02-27 16:46:51 +0500

    completed execise 1 and 2

---
+++ module6-unit-testing/unit-testing/src/main/java/com/example/unittesting/LeapYearCalculator.java
@@ -0,0 +1,7 @@
+package com.example.unittesting;
+
+public class LeapYearCalculator {
+    public boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
